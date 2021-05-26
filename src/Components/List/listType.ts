362dad157d8f6890f65ed7548f5e7e96af8f2bdf
quintui <rscrafter1234@icloud.com>
2021-05-26 14:27:22 +0300

    Added docs for Photo component and List component

--- src/Components/List/listType.ts
+++ src/Components/List/listType.ts
@@ -4,8 +4,17 @@ export type ListContent = {
 };
 
 export interface ListProps {
+  /**
+   *Array of objects comprising name: string (name of each list item) id: string
+   */
   list: ListContent[];
+  /**
+   *Callback which return all your list item object
+   */
   callback: (item: ListContent) => void;
+  /**
+   *Font size of each list item
+   */
   fontSize?: number;
 }
 
