30b4c8c558f383224f23d42a6103da2c45bfacb0
Artem <artemsimutin@gmail.com>
2021-05-27 21:54:28 +0300

    NN11 Fixed bug with displaying stepper and modded button

--- src/Components/Stepper/Stepper.stories.tsx
+++ src/Components/Stepper/Stepper.stories.tsx
@@ -1,37 +1,8 @@
 import { Story } from '@storybook/react';
 import React from 'react';
+import { doneItems, testArray } from './items';
 import { Stepper } from './Stepper';
-import { StepperElement, StepperProps } from './StepperTypes';
-
-export const doneItems: StepperElement[] = [
-  { id: 'dsjhtfs67u', value: 'string' },
-  { id: '2tarffuyewjk4', value: 'string' },
-  { id: '348535j4hn578fsd', value: 'string' },
-  { id: '12sdf', value: 'string' },
-  { id: '13sdf', value: 'string' },
-  { id: 'hfghfg465', value: 'string' },
-  { id: 'gdfdfggfd', value: 'string' },
-  { id: 'sffsd3456', value: 'string' },
-  { id: '11kkogfposbhjv3', value: 'string' },
-];
-
-export const testArray: StepperElement[] = [
-  { id: '11kkogfposbhjv3', value: 'string' },
-  { id: '12sdf', value: 'string' },
-  { id: '13sdf', value: 'string' },
-  { id: '121233', value: 'string' },
-  { id: '1456456fdsg', value: 'string' },
-  { id: 'dgdfg2345', value: 'string' },
-  { id: 'hfghfg465', value: 'string' },
-  { id: 'gdfdfggfd', value: 'string' },
-  { id: 'sffsd3456', value: 'string' },
-  { id: '14kfgh2234', value: 'string' },
-  { id: 's123adsffd', value: 'string' },
-  { id: 'kjdfy876', value: 'string' },
-  { id: 'dsjhtfs67u', value: 'string' },
-  { id: '2tarffuyewjk4', value: 'string' },
-  { id: '348535j4hn578fsd', value: 'string' },
-];
+import { StepperProps } from './StepperTypes';
 
 export default {
   title: 'Common/Stepper',
